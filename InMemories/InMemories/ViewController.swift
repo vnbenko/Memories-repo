a36869366a0fb294cc8988569b2fc4a93b149eaf
Vladimir Benko <vnbenko@gmail.com>
2021-10-05 19:49:20 +0300

    TASK-2_create_registration_screen

--- InMemories/InMemories/ViewController.swift
+++ InMemories/InMemories/ViewController.swift
@@ -1,12 +1,91 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let photoButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "photoButton").withRenderingMode(.alwaysOriginal), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let emailTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Email"
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        textField.font = UIFont.systemFont(ofSize: 14)
+        textField.borderStyle = .roundedRect
+        return textField
+    }()
+    
+    let userNameTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Username"
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        textField.font = UIFont.systemFont(ofSize: 14)
+        textField.borderStyle = .roundedRect
+        return textField
+    }()
+    
+    let passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Password"
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        textField.font = UIFont.systemFont(ofSize: 14)
+        textField.borderStyle = .roundedRect
+        return textField
+    }()
+    
+    let signUpButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Sign Up", for: .normal)
+        button.backgroundColor = UIColor(red: 149/255, green: 204/255, blue: 244/255, alpha: 1)
+        button.layer.cornerRadius = 5
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(.white, for: .normal)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(photoButton)
+        NSLayoutConstraint.activate([
+            photoButton.heightAnchor.constraint(equalToConstant: 140),
+            photoButton.widthAnchor.constraint(equalToConstant: 140),
+            photoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            photoButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 40)
+        ])
+        
+        setupInputsFields()
+
     }
-
-
+    
+    fileprivate func setupInputsFields() {
+        
+        let stackView = UIStackView(arrangedSubviews: [
+                                        emailTextField,
+                                        userNameTextField,
+                                        passwordTextField,
+                                        signUpButton])
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.distribution = .fillEqually
+        stackView.axis = .vertical
+        stackView.spacing = 10
+        
+        view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: photoButton.bottomAnchor, constant: 20),
+            stackView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 40),
+            stackView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -40),
+            stackView.heightAnchor.constraint(equalToConstant: 200)
+        ])
+    }
+    
 }
 
