ec62b96cf5e41ca99bae8d1aec2d551aab0939fe
Vladimir Benko <vnbenko@gmail.com>
2021-10-06 00:08:19 +0300

    TASK-4. Install Firebase and prepare registration form

--- InMemories/InMemories/ViewController.swift
+++ InMemories/InMemories/ViewController.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
     
@@ -14,6 +15,8 @@ class ViewController: UIViewController {
         textField.backgroundColor = UIColor(white: 0, alpha: 0.03)
         textField.font = UIFont.systemFont(ofSize: 14)
         textField.borderStyle = .roundedRect
+        
+        textField.addTarget(self, action: #selector(handleTextInputChange), for: .editingChanged)
         return textField
     }()
     
@@ -23,6 +26,8 @@ class ViewController: UIViewController {
         textField.backgroundColor = UIColor(white: 0, alpha: 0.03)
         textField.font = UIFont.systemFont(ofSize: 14)
         textField.borderStyle = .roundedRect
+        
+        textField.addTarget(self, action: #selector(handleTextInputChange), for: .editingChanged)
         return textField
     }()
     
@@ -32,6 +37,8 @@ class ViewController: UIViewController {
         textField.backgroundColor = UIColor(white: 0, alpha: 0.03)
         textField.font = UIFont.systemFont(ofSize: 14)
         textField.borderStyle = .roundedRect
+        
+        textField.addTarget(self, action: #selector(handleTextInputChange), for: .editingChanged)
         return textField
     }()
     
@@ -42,6 +49,9 @@ class ViewController: UIViewController {
         button.layer.cornerRadius = 5
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
         button.setTitleColor(.white, for: .normal)
+        
+        button.addTarget(self, action: #selector(handleSignUp), for: .touchUpInside)
+        button.isEnabled = false
         return button
     }()
     
@@ -49,7 +59,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         view.addSubview(photoButton)
-
+        
         photoButton.anchor(top: view.topAnchor, paddingTop: 40,
                            left: nil, paddingLeft: 0,
                            right: nil, paddingRight: 0,
@@ -73,13 +83,42 @@ class ViewController: UIViewController {
         stackView.spacing = 10
         
         view.addSubview(stackView)
-
+        
         stackView.anchor(top: photoButton.bottomAnchor, paddingTop: 20,
                          left: view.leftAnchor, paddingLeft: 40,
                          right: view.rightAnchor, paddingRight: 40,
                          bottom: nil, paddingBottom: 0,
                          width: 0, height: 200)
-    }    
+    }
+    
+    @objc func handleSignUp() {
+        guard let email = emailTextField.text, !email.isEmpty  else { return }
+        guard let userName = userNameTextField.text, !userName.isEmpty else { return }
+        guard let password = passwordTextField.text, !password.isEmpty else { return }
+        Auth.auth().createUser(withEmail: email, password: password) { (result, error) in
+            if let error = error {
+                print("Failed to create user:", error)
+                return
+            }
+            
+            print("Successfully created user:",result?.user.uid ?? "")
+        }
+    }
+    
+    @objc func handleTextInputChange() {
+        
+        let isValid = emailTextField.text?.count ?? 0 > 0 && userNameTextField.text?.count ?? 0 > 0 &&
+            passwordTextField.text?.count ?? 0 > 0
+        
+        if isValid {
+            signUpButton.isEnabled = true
+            signUpButton.backgroundColor = UIColor.rgb(17, 154, 237)
+        } else {
+            signUpButton.isEnabled = false
+            signUpButton.backgroundColor = UIColor.rgb(149, 204, 244)
+        }
+        
+    }
 }
 
 
