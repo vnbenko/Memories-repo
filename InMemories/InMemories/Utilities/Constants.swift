3c3755222571177fc036c0e193dbc3c9540c4c8f
Vladimir Benko <vnbenko@gmail.com>
2021-11-22 19:53:55 +0300

    TASK-59. Rename project

--- InMemories/InMemories/Utilities/Constants.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-class Constants {
-    
-    static let shared = Constants()
-    
-    let databaseUrlString = "https://inmemories-d02b2-default-rtdb.europe-west1.firebasedatabase.app"
-}
---
+++ Memories/Memories/Utilities/Constants.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+class Constants {
+    
+    static let shared = Constants()
+    
+    let databaseUrlString = "https://memories-34722-default-rtdb.europe-west1.firebasedatabase.app"
+}
